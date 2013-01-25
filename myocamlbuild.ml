b6aab321642a2669510b0324bf72d41558465595
Khoo Yit Phang <khooyp@cs.umd.edu>
2013-01-24 22:28:17 -0500

    Enable backtrace printing in "make repl".

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,6 +4,7 @@ let _ = dispatch begin function
     | After_rules ->
         (* missing rule as of OCamlbuild 4.00.1 *)
         flag [ "ocaml"; "rectypes"; "pack" ] (A "-rectypes");
+        flag [ "ocaml"; "debug"; "link"; "toplevel" ] (A "-g");
     | _ ->
         ()
 end
