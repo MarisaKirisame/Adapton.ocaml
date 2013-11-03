1d240dd673a08feafb3205ea461c34c63aeeabd7
James Parker <jp@jamesparker.me>
2013-11-03 14:09:41 -0500

    updated Unit

--- Source/Adapton/Types.ml
+++ Source/Adapton/Types.ml
@@ -76,10 +76,10 @@ module Tuple4 (A : Hashtbl.SeededHashedType) (B : Hashtbl.SeededHashedType) (C :
     let equal ( a, b, c, d as x ) ( a', b', c', d' as x' ) = x == x' || A.equal a a' && B.equal b b' && C.equal c c' && D.equal d d'
 end
 
-module Unit : Hashtbl.HashedType = struct
-    type t = ()
+module Unit : Hashtbl.SeededHashedType = struct
+    type t = unit
+    let hash _ _ = 1
     let equal = (=)
-    let hash _ = 1
 end
 
 (** Counter. *)
