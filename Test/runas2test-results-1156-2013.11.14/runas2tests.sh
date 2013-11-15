bd7c520864a1c554602583f2b4db5856df5f9709
Matthew A Hammer <hammer@cs.umd.edu>
2013-11-15 09:09:10 -0500

    more experiments

---
+++ Test/runas2test-results-1156-2013.11.14/runas2tests.sh
@@ -0,0 +1,16 @@
+
+BIN=../../_product
+
+touch runas2testlazysabidi.csv
+touch runas2testeagersatotalorder.csv
+touch runas2testnonsaeager.csv
+
+for sht in 2 3 4 5 6 7 10 12 13 14 15; do
+    for samp in 1 2 3 4 5 6 7 8; do
+    echo "sheet depth=$sht/20; same number=$samp"
+    args="--Random.self_init --stats-test $sht"
+    $BIN/runas2testlazysabidi.native $args --stats-out runas2testlazysabidi.csv  
+    $BIN/runas2testnonsaeager.native $args --stats-out runas2testnonsaeager.csv 
+    $BIN/runas2testeagersatotalorder.native $args --stats-out runas2testeagersatotalorder.csv
+    done   
+done
